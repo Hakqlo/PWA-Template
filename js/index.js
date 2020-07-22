c91c7794e8943e98e9aaa7fcbd2f7606f85be7eb
Momijiichigo <ichigomomiji436@gmail.com>
2020-07-22 19:29:33 +0900

    ogp

--- js/index.js
+++ js/index.js
@@ -5,6 +5,6 @@ import {init as basicInit} from "./baseFunctions";
 import installProcess from './install';
 //import '../node_modules/dialog-polyfill/dist/dialog-polyfill.css';
 import '../css/main.scss';
-import '../css/startupApp.css';
+//import '../css/startupApp.css';
 basicInit(window);
 installProcess(window);
